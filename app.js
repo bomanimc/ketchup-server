78582944294fc758bab9111711171d61f4418455
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2015-11-30 23:54:15 -0600

    More explanative logging

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ var sendSMS = function(numVal, bodyVal) {
 	    to: numVal,
 	    from: twilioNum,
 	}, function(err, message) {
+		console.log("An error occurred with " + numVal);
 	    console.log(err + " " + message);
 	});
 };
